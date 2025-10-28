2b6779959964086b8168579b7631d748320d37ca
FatemehYafteh <fatemmehyafteh@gmail.com>
2025-10-28 01:39:27 -0700

    last commit

---
+++ freelnacer-ap-frontend/src/services/authService.js
@@ -0,0 +1,31 @@
+import http from "./httpService";
+
+export function getOtp(data) {
+  return http.post("/user/get-otp", data).then(({ data }) => data.data);
+}
+
+export function checkOtp(data) {
+  return http.post("/user/check-otp", data).then(({ data }) => data.data);
+}
+
+export function completeProfile(data) {
+  return http
+    .post("/user/complete-profile", data)
+    .then(({ data }) => data.data);
+}
+export function getUser() {
+  return http.get("/user/profile").then(({ data }) => data.data);
+}
+export function getUsersApi() {
+  return http.get("/admin/users").then(({ data }) => data.data);
+}
+export function changeUserStatusApi(userId, status) {
+  return http
+    .patch(`/admin/users/${userId}`, { status })
+    .then(({ data }) => data.data);
+}
+
+// 🔹 خروج کاربر
+export function logoutApi() {
+  return http.post("/user/logout").then(({ data }) => data.data);
+}
